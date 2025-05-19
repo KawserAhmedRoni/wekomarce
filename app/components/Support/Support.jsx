0679fb6ae51bf1ec19a92168d63ef129c46dd9d6
noyononline <noyononlinen@gmail.com>
2025-05-19 13:26:39 +0600

    quality ensure page complate

---
+++ app/components/Support/Support.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { LuBadgeCheck } from "react-icons/lu";
+import { PiFireTruck } from "react-icons/pi";
+import { GiCardboardBox } from "react-icons/gi";
+import { GrCreditCard } from "react-icons/gr";
+
+const Support = () => {
+  return (
+    <div className="py-5 vw-100">
+      <div className="container">
+        <div className="row">
+          <div className="d-flex justify-content-between gap-3">
+            <div className="col-4 wearesupport d-flex justify-content-center  align-items-center gap-2 mx-auto">
+              <LuBadgeCheck className="fs-5 icons" />
+              <p className="fs-6 text-capitalize fw-normal mb-0">
+                Premium Quality
+              </p>
+            </div>
+            <div className="col-4 wearesupport d-flex justify-content-center  align-items-center gap-2 mx-auto">
+              <GiCardboardBox className="fs-5 icons" />
+              <p className="fs-6 text-capitalize fw-normal mb-0">Easy Return</p>
+            </div>
+            <div className="col-4 wearesupport d-flex justify-content-center  align-items-center gap-2 mx-auto">
+              <PiFireTruck className="fs-5 icons" />
+              <p className="fs-6 text-capitalize fw-normal mb-0">
+                National Delivery
+              </p>
+            </div>
+            <div className="col-4 wearesupport d-flex justify-content-center  align-items-center gap-2 mx-auto">
+              <GrCreditCard className="fs-5 icons" />
+              <p className="fs-6 text-capitalize fw-normal mb-0">
+                Safe Payment
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Support;
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Banner from "./components/Banner/banner";
 import Header from "./components/Header/Header";
+import Support from "./components/Support/Support";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Banner />
+      <Support />
       <div className="bg-danger">home</div>;
     </main>
   );
